ed1ed22f84ff4d55460e9abe8c7e1912e8987a26
doujohn097-code <doujohn097@gmail.com>
2026-08-04 08:36:57 +0100

    Upload via git-zip - tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          pink: '#ff6ec7',
+          purple: '#b25cff',
+          blue: '#4facfe',
+        },
+        bg: '#0f0f12',
+        surface: '#18181d',
+        'surface-light': '#25252e',
+      },
+      fontFamily: {
+        sans: ['Almarai', 'Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
